6eca70ddbe4b21ca2940428d06026dc8aaebd00c
kralyatron <budin.vadim@mail.ru>
2018-12-27 14:46:59 +0900

    comething new

--- BudishchevVadimAndMZakharovaMilana/add_reception.php
+++ BudishchevVadimAndMZakharovaMilana/add_reception.php
@@ -80,7 +80,7 @@
 					$SQLresult = mysqli_query($link,$SQLquery);
 					while ($result = mysqli_fetch_array($SQLresult,MYSQLI_NUM))
 					{
-						printf('<option>%d</option>',$result[0],);
+						printf('%d',$result[0],);
 					}
 					mysqli_free_result($SQLresult);
 					mysqli_close($link);
